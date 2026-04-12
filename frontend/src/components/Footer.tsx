11a6db80193a055cefc91ee88337862b1736c010
nikshaan <shettynikshaan@gmail.com>
2026-04-12 21:35:25 +0530

    update: footer links

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,8 +3,8 @@ export default function Footer() {
     <footer className="site-footer">
       &copy; 2026 Vipul Sharma &middot;{' '}
       <a href="/feed.xml">RSS</a> &middot;{' '}
-      <a href="https://github.com/vipul" target="_blank" rel="noopener noreferrer">GitHub</a> &middot;{' '}
-      <a href="https://twitter.com/vipul" target="_blank" rel="noopener noreferrer">Twitter</a>
+      <a href="https://github.com/vipul-sharma20" target="_blank" rel="noopener noreferrer">GitHub</a> &middot;{' '}
+      <a href="https://x.com/vipul_sharma" target="_blank" rel="noopener noreferrer">Twitter</a>
     </footer>
   );
 }
